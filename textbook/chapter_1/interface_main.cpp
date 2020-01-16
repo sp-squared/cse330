07ebe84ca8c7ffc5b78d6209c5f43d62db3ae439
Colin A. Morris <colin.morris.r@gmail.com>
2020-01-15 23:33:16 -0800

    Interface Implementation txt book

---
+++ textbook/chapter_1/interface_main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "interface.h"
+using namespace std;
+int main(){
+        IntCell num1;
+        num1.IntWrite(6);
+        cout << "Num 1 = " << num1.IntRead() << endl;
+return 0;
+}
